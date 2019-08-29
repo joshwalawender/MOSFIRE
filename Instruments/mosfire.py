514496556842857c2a048f20f02a3fd9add55c6a
Josh Walawender <jmwalawender@gmail.com>
2019-08-28 15:57:58 -1000

    Add checking functions

--- Instruments/mosfire.py
+++ Instruments/mosfire.py
@@ -1,6 +1,7 @@
 #!/usr/env/python
 
 ## Import General Tools
+import sys
 from pathlib import Path
 import logging
 import yaml
@@ -112,6 +113,7 @@ def set_mode(filter, mode):
 
 
 def go_dark():
+    # See darkeff instead!!!
     if is_dark():
         log.info('Already Dark (mode = {get_mode()})')
         return True
@@ -119,6 +121,59 @@ def go_dark():
     current = get_mode()
     setmode('Dark', current[1])
     return is_dark()
+
+
+def grating_shim_ok():
+    return get('mosfire', 'MGSSTAT') == 'OK':
+
+
+def grating_turret_ok():
+    return get('mosfire', 'MGTSTAT') == 'OK':
+
+
+def grating_ok():
+    return get('mosfire', 'GRATSTAT') == 'OK':
+
+
+def filter1_ok():
+    return get('mosfire', 'MF1STAT') == 'OK':
+
+
+def filter2_ok():
+    return get('mosfire', 'MF2STAT') == 'OK':
+
+
+def filters_ok():
+    return get('mosfire', 'FILTSTAT') == 'OK':
+
+
+def fcs_ok():
+    return get('mosfire', 'FCSSTAT') == 'OK':
+
+
+def pupil_rotator_ok():
+    return get('mosfire', 'MPRSTAT') == 'OK':
+
+
+def trapdoor_ok():
+    return dustcover_ok()
+
+
+def dustcover_ok():
+    return get('mosfire', 'MDCSTAT') == 'OK':
+
+
+def check_mechanisms():
+    mechs = ['filter1', 'filter2', 'fcs', 'grating_shim', 'grating_turret',
+             'pupil_rotator', 'trapdoor']
+    for mech in mechs:
+        statusfn = getattr(sys.modules[__name__], f'{mech}_ok')
+        ok = statusfn()
+        if ok is False:
+            log.error(f'{mech} status is not ok')
+            log.error(f'Please address the problem, then re-run the checkout.')
+            return False
+    return True
 
 
 ##-------------------------------------------------------------------------
@@ -365,7 +420,8 @@ def checkout_quick(interactive=True):
     * Inspect the dark image
     * Create an 2.7x46 long slit and image it, verify bar positions
     * Create an 0.7x46 long slit and image it, verify bar positions
-    * With the hatch closed change the observing mode to J-imaging
+    * With the hatch closed change the observing mode to J-imaging, verify
+         mechanisms are ok
     * Quick Dark
     '''
     intromsg = 'This script will do a quick checkout of MOSFIRE.  It should '\
@@ -385,3 +441,9 @@ def checkout_quick(interactive=True):
     # Verify that the instrument is "dark"
     if not is_dark():
         go_dark()
+    check_mechanisms()
+    # Verify Dark Image
+    # Create an 2.7x46 long slit and image it, verify bar positions
+    # Create an 0.7x46 long slit and image it, verify bar positions
+    # Change the observing mode to J-imaging, verify mechanisms
+    # Quick Dark
