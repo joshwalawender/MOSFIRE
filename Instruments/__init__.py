001337880cd0de6756628f2479c18f1c3819a471
Josh Walawender <jmwalawender@gmail.com>
2019-09-06 17:02:24 -1000

    Update format

--- Instruments/__init__.py
+++ Instruments/__init__.py
@@ -11,8 +11,7 @@ def create_log(name='KeckInstrument', loglevel=logging.INFO):
     ## Set up console output
     LogConsoleHandler = logging.StreamHandler()
     LogConsoleHandler.setLevel(loglevel)
-    LogFormat = logging.Formatter('%(asctime)s %(levelname)8s: %(message)s',
-                                  datefmt='%Y-%m-%d %H:%M:%S.%f')
+    LogFormat = logging.Formatter('%(asctime)s %(levelname)8s: %(message)s')
     LogConsoleHandler.setFormatter(LogFormat)
     log.addHandler(LogConsoleHandler)
     ## Set up file output
