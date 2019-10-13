3a4e9ec8c6b192aaad1da345716a77eb22faa078
Josh Walawender <jmwalawender@gmail.com>
2019-10-13 12:36:10 -1000

    Allow skipping of rot positions and choose obsmode

--- Instruments/mosfire/scripts.py
+++ Instruments/mosfire/scripts.py
@@ -4,18 +4,22 @@ from .detector import *
 from time import sleep
 
 
-def measure_FCS_flexure(rotpposn):
+def measure_FCS_flexure(rotpposn, obsmode='H-spectroscopy'):
     set_rotpposn(rotpposn)
-    set_obsmode('H-spectroscopy')
+    set_obsmode(obsmode)
     waitfor_FCS()
     take_exposure(wait=True)
     go_dark(wait=False)
 
 
-def measure_FCS_flexure_set(reverse=False):
+def measure_FCS_flexure_set(reverse=False, skip=None, obsmode='H-spectroscopy'):
     rotpposns = [-360, -315, -270, -225, -180, -135, -90, -45, 0, 45]
     if reverse is True:
         rotpposns.reverse()
+    if skip is not None:
+        for i in range(skip):
+            skipped = rotpposns.pop(0)
+            log.info(f'Skipping ROTPPOSN = {skipped}')
     for rotpposn in rotpposns:
-        measure_FCS_flexure(rotpposn)
+        measure_FCS_flexure(rotpposn, obsmode=obsmode)
         sleep(1)
