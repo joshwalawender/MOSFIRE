f8674d452e99b6a1f2762576402189c0cdebb22d
Josh Walawender <jmwalawender@gmail.com>
2019-10-31 13:42:45 -1000

    Organize and add ECHANG control

--- Instruments/hires/mechs.py
+++ Instruments/hires/mechs.py
@@ -2,7 +2,7 @@ from .core import *
 
 
 ##-------------------------------------------------------------------------
-## HIRES Mechanisms
+## Lights, Door, Collimator
 ##-------------------------------------------------------------------------
 def lights_are_on():
     """Returns True if lights are on in the enclosure.
@@ -56,6 +56,9 @@ def collimator():
     return result
 
 
+##-------------------------------------------------------------------------
+## Covers
+##-------------------------------------------------------------------------
 def set_covers(dest, wait=True):
     """Opens or closes all internal covers.
     
@@ -112,6 +115,9 @@ def close_covers(wait=True):
     set_covers('closed', wait=wait)
 
 
+##-------------------------------------------------------------------------
+## Slit, Decker, Filters
+##-------------------------------------------------------------------------
 def open_slit(wait=True):
     """Open the slit jaws.
     """
@@ -140,6 +146,27 @@ def set_filters(fil1name, fil2name, wait=True):
     set('hires', 'fil2name', fil2name, wait=wait)
 
 
+def set_tvfilter(tvf1name, wait=True):
+    log.info(f'Setting TVF1NAME to {tvf1name}')
+    set('hires', 'TVF1NAME', tvf1name, wait=wait)
+
+
+##-------------------------------------------------------------------------
+## Focus
+##-------------------------------------------------------------------------
+def set_cafraw(cafraw, wait=True):
+    log.info(f'Setting CAFRAW to {cafraw:.3f}')
+    set('hires', 'cafraw', cafraw, wait=wait)
+
+
+def set_cofraw(cofraw, wait=True):
+    log.info(f'Setting COFRAW to {cofraw:.3f}')
+    set('hires', 'cofraw', cofraw, wait=wait)
+
+
+##-------------------------------------------------------------------------
+## Grating Angles
+##-------------------------------------------------------------------------
 def xdang():
     return get('hires', 'XDANGL', mode=float)
 
@@ -167,7 +194,7 @@ def set_xdang(dest, simple=False, threshold=0.5, step=0.5):
         log.debug(f"Making final move to {dest:.3f}")
         set('hires', 'XDANGL', dest, wait=True)
     log.info(f"Done.  XDANGL = {xdang():.3f} deg")
-    return XDANG
+    return xdang()
 
 
 def set_xdraw(dest, simple=False, threshold=2000, step=2000):
@@ -189,20 +216,58 @@ def set_xdraw(dest, simple=False, threshold=2000, step=2000):
         log.debug(f"Making final move to {dest:.3f}")
         set('hires', 'XDRAW', dest, wait=True)
     log.debug(f"Done.  XDRAW = {xdraw():.3f} steps")
-    return XDRAW
+    return xdraw()
+
+def echang():
+    return get('hires', 'ECHANGL', mode=float)
 
 
-def set_cafraw(cafraw, wait=True):
-    log.info(f'Setting CAFRAW to {cafraw:.3f}')
-    set('hires', 'cafraw', cafraw, wait=wait)
+def echraw():
+    return get('hires', 'ECHRAW', mode=int)
 
 
-def set_cofraw(cofraw, wait=True):
-    log.info(f'Setting COFRAW to {cofraw:.3f}')
-    set('hires', 'cofraw', cofraw, wait=wait)
+def set_echang(dest, simple=False, threshold=0.5, step=0.5):
+    log.info(f'Moving ECHANGL to {dest:.3f} deg')
+    if simple is True:
+        log.debug(f"Making simple move to {dest:.3f}")
+        set('hires', 'ECHANGL', dest, wait=True)
+    else:
+        delta = dest - echang()
+        log.debug(f'Total move is {delta:.3f} deg')
+        if abs(delta) > threshold:
+            nsteps = int(np.floor(abs(delta) / step))
+            log.debug(f"Will move in {nsteps+1} steps")
+            for i in range(nsteps):
+                movedest = echang() + np.sign(delta)*step
+                log.debug(f"Making intermediate move to {movedest:.3f}")
+                set('hires', 'ECHANGL', movedest, wait=True)
+                sleep(1)
+        log.debug(f"Making final move to {dest:.3f}")
+        set('hires', 'ECHANGL', dest, wait=True)
+    log.info(f"Done.  ECHANGL = {echang():.3f} deg")
+    return echang()
 
 
-def set_tvfilter(tvf1name, wait=True):
-    log.info(f'Setting TVF1NAME to {tvf1name}')
-    set('hires', 'TVF1NAME', tvf1name, wait=wait)
+def set_echraw(dest, simple=False, threshold=2000, step=2000):
+    log.info(f'Moving ECHRAW to {dest:.3f} counts')
+    if simple is True:
+        log.debug(f"Making simple move to {dest:.3f}")
+        set('hires', 'ECHRAW', dest, wait=True)
+    else:
+        delta = dest - echraw()
+        log.debug(f'Total move is {delta:.3f} counts')
+        if abs(delta) > threshold:
+            nsteps = int(np.floor(abs(delta) / step))
+            log.debug(f"Will move in {nsteps+1} steps")
+            for i in range(nsteps):
+                movedest = echraw() + np.sign(delta)*step
+                log.debug(f"Making intermediate move to {movedest:.3f}")
+                set('hires', 'ECHRAW', movedest, wait=True)
+                sleep(1)
+        log.debug(f"Making final move to {dest:.3f}")
+        set('hires', 'ECHRAW', dest, wait=True)
+    log.debug(f"Done.  ECHRAW = {echraw():.3f} steps")
+    return echraw()
+
+
 
