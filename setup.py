d68590c56ef0b8a3c41eb2b5a0131ea0a564897f
Josh Walawender <jmwalawender@gmail.com>
2020-01-14 10:08:39 -1000

    Rename for PEP8 compliance

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "Instruments",
+    name = "KeckInstruments",
     version='0.0.1',
     author='Josh Walawender',
     packages = find_packages(),
