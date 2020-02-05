c4aa9841dcd2e1a62de97e89e3003cf4a71286d8
Josh Walawender <jmwalawender@gmail.com>
2020-02-05 09:46:40 -1000

    Remove unused function

--- instruments/instrument.py
+++ instruments/instrument.py
@@ -33,37 +33,3 @@ def create_log(name='KeckInstrument', loglevel=logging.INFO, logfile=None):
             log.addHandler(LogFileHandler)
     
     return log
-
-
-##-------------------------------------------------------------------------
-## Import KTL python
-##-------------------------------------------------------------------------
-# Wrap ktl import in try/except so that we can maintain test case or
-# simulator version of functions on machines without KTL installed.
-
-def connect_to_ktl(instrumentName, serviceNames, noactions=False):
-    log = logging.getLogger(instrumentName)
-    if noactions is True:
-        ktl = None
-    else:
-        try:
-            import ktl
-            from ktl import Exceptions as ktlExceptions
-        except ModuleNotFoundError:
-            ktl = None
-        except ktlExceptions.ktlError:
-            ktl = None
-
-    # Connect to KTL Services
-    services = {}
-    if ktl is not None:
-        for service in serviceNames:
-            try:
-                services[service] = ktl.Service(service)
-            except ktlExceptions.ktlError as e:
-                log.error(f"ERROR: Failed to connect to service {service}")
-                log.error(e)
-
-        log.info(f'Connected {len(services)} / {len(serviceNames)} services')
-    return services
-
