4a9ccdbd5fb216b41278bd00ec75b2590fe629cd
Josh Walawender <jmwalawender@gmail.com>
2020-02-07 11:03:46 -1000

    Add rotator

--- instruments/mosfire/core.py
+++ instruments/mosfire/core.py
@@ -79,3 +79,63 @@ def dustcover_ok():
 #     for mech in mechs:
 #         statusfn = getattr(sys.modules[__name__], f'{mech}_ok')
 #         statusfn()
+
+
+
+##-----------------------------------------------------------------------------
+## Rotator Control
+##-----------------------------------------------------------------------------
+def _set_rotpposn(rotpposn, skipprecond=False, skippostcond=False):
+    '''Set the rotator position in stationary mode.
+    
+    This only tries to set the position once, use `set_rotpposn` in practice
+    as it makes multiple attempts which seems to be more reliable.
+    '''
+    this_function_name = inspect.currentframe().f_code.co_name
+    log.debug(f"Executing: {this_function_name}")
+
+    ##-------------------------------------------------------------------------
+    ## Pre-Condition Checks
+    if skipprecond is True:
+        log.debug('Skipping pre condition checks')
+    else:
+        pass
+    
+    ##-------------------------------------------------------------------------
+    ## Script Contents
+
+    log.info(f'Setting ROTPPOSN to {rotpposn:.1f}')
+    ROTDESTkw = ktl.cache(service='dcs', keyword='ROTDEST')
+    ROTDESTkw.write(float(rotpposn))
+    sleep(1)
+    ROTMODEkw = ktl.cache(service='dcs', keyword='ROTMODE')
+    ROTMODEkw.write('stationary')
+    sleep(1)
+    
+    ##-------------------------------------------------------------------------
+    ## Post-Condition Checks
+    if skippostcond is True:
+        log.debug('Skipping post condition checks')
+    else:
+        log.info(f'Waiting for rotator to be "in position"')
+        ROTSTATkw = ktl.cache(service='dcs', keyword='ROTSTAT')
+        ROTSTATkw.monitor()
+        while str(ROTSTATkw) != 'in position':
+            log.debug(f'ROTSTAT = "{ROTSTATkw}"')
+            sleep(2)
+
+    return None
+
+
+def set_rotpposn(rotpposn):
+    '''Set the rotator position in stationary mode.  Performs a single retry if
+    a ktlError is raised.
+    '''
+    try:
+        _set_rotpposn(rotpposn)
+    except ktlExceptions.ktlError as e:
+        log.warning(f"Failed to set rotator")
+        log.warning(e)
+        sleep(2)
+        log.info('Trying again ...')
+        _set_rotpposn(rotpposn)
