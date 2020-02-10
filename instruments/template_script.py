f32be517de95114358a6540a4d47a07f16b68c1f
Josh Walawender <jmwalawender@gmail.com>
2020-02-10 10:37:56 -1000

    Scripts should have condition checks

--- instruments/template_script.py
+++ instruments/template_script.py
@@ -55,8 +55,29 @@ log.addHandler(LogConsoleHandler)
 ## The Instrument Script
 ##-------------------------------------------------------------------------
 def instrument_script():
+    this_script_name = inspect.currentframe().f_code.co_name
+    log.debug(f"Executing: {this_script_name}")
 
+    ##-------------------------------------------------------------------------
+    ## Pre-Condition Checks
+    if skipprecond is True:
+        log.debug('Skipping pre condition checks')
+    else:
+        if condition1 is not True:
+            raise FailedCondition('description of failure')
+    
+    ##-------------------------------------------------------------------------
+    ## Script Contents
 
+    # ----> insert instrument script here <----
+    
+    ##-------------------------------------------------------------------------
+    ## Post-Condition Checks
+    if skippostcond is True:
+        log.debug('Skipping post condition checks')
+    else:
+        if condition2 is not True:
+            raise FailedCondition('description of failure')
 
 
 if __name__ == '__main__':
