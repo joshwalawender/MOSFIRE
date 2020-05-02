a56ccfa65974d57906e20f221c75ffa30f94b814
Josh Walawender <jmwalawender@gmail.com>
2020-05-01 16:30:56 -1000

    More calibrations work

--- instruments/mosfire/scripts/calibration.py
+++ instruments/mosfire/scripts/calibration.py
@@ -71,10 +71,73 @@ log.addHandler(LogConsoleHandler)
 # log.addHandler(LogFileHandler)
 
 
+
 ##-------------------------------------------------------------------------
-## The Instrument Script
+## Sub-function: Take Arcs
 ##-------------------------------------------------------------------------
-def take_mask_calibrations(mask, filter, skipprecond=False, skippostcond=True):
+def take_arcs(filt, cfg):
+    # Close hatch
+    go_dark()
+    close_hatch()
+    # Take Ne arcs
+    if int(cfg.get(filt, "ne_arc_count", fallback=0)) > 0:
+        set_obsmode(f"{filt}-spectroscopy")
+        exptime = float(cfg.get(filt, "ne_arc_exptime", fallback=1))
+        Ne_lamp('on')
+        for i in range(int(cfg.get(filt, "ne_arc_count", fallback=0))):
+            take_exposure(exptime=exptime,
+                          coadds=int(cfg.get(filt, "ne_arc_coadds", fallback=1)),
+                          sampmode=cfg.get(filt, "ne_arc_sampmode", fallback='CDS'),
+                          wait=True)
+        Ne_lamp('off')
+    # Take Ar arcs
+    if int(cfg.get(filt, "ar_arc_count", fallback=0)) > 0:
+        set_obsmode(f"{filt}-spectroscopy")
+        exptime = float(cfg.get(filt, "ar_arc_exptime", fallback=1))
+        Ar_lamp('on')
+        for i in range(int(cfg.get(filt, "ar_arc_count", fallback=0))):
+            take_exposure(exptime=exptime,
+                          coadds=int(cfg.get(filt, "ar_arc_coadds", fallback=1)),
+                          sampmode=cfg.get(filt, "ar_arc_sampmode", fallback='CDS'),
+                          wait=True)
+        Ar_lamp('off')
+    # Go Dark
+    go_dark()
+
+
+##-------------------------------------------------------------------------
+## Sub-function: Take Flats
+##-------------------------------------------------------------------------
+def take_flats(filt, cfg):
+    # Open Hatch
+    open_hatch()
+    # Turn on dome flat lamps
+    dome_flat_lamps(filt)
+    # Set mode
+    set_obsmode(f"{filt}-spectroscopy")
+    # Take flats
+    exptime = float(cfg.get(filt, "flat_exptime", fallback=10))
+    for i in range(int(cfg.get(filt, "flat_count", fallback=9))):
+        take_exposure(exptime=exptime,
+                      coadds=int(cfg.get(filt, "flat_coadds", fallback=1)),
+                      sampmode=cfg.get(filt, "flat_sampmode", fallback='CDS'),
+                      wait=True)
+    # Turn off dome flat lamps
+    if int(cfg.get(filt, "flatoff_count", fallback=0)) > 0:
+        dome_flat_lamps('off')
+    # Take lamp off flats
+    for i in range(int(cfg.get(filt, "flatoff_count", fallback=0))):
+        take_exposure(exptime=exptime,
+                      coadds=int(cfg.get(filt, "flat_coadds", fallback=1)),
+                      sampmode=cfg.get(filt, "flat_sampmode", fallback='CDS'),
+                      wait=True)
+    go_dark()
+
+
+##-------------------------------------------------------------------------
+## Take Calibrations for a Single Mask for a List of Bands
+##-------------------------------------------------------------------------
+def take_mask_calibrations(mask, filters, skipprecond=False, skippostcond=True):
     this_script_name = inspect.currentframe().f_code.co_name
     log.debug(f"Executing: {this_script_name}")
 
@@ -84,72 +147,43 @@ def take_mask_calibrations(mask, filter, skipprecond=False, skippostcond=True):
     if skipprecond is True:
         log.debug('Skipping pre condition checks')
     else:
+        mechanisms_ok()
         # Configuration file exists
         if cfg_file.exists() is not True:
             raise FailedCondition(f'Could not find configuration file: {args.config}')
         # Mask input is a mask object
         if not isinstance(mask, Mask):
             mask = Mask(mask)
-        instrument_is_MOSFIRE()
-        mechanisms_ok()
+        # Input filters is list
+        if type(filters) is str:
+            filters = [filters]
 
     ##-------------------------------------------------------------------------
     ## Script Contents
 
-    # Read configuration file
+    # Read calibration configuration file
     cfg = configparser.ConfigParser()
     cfg.read(cfg_file)
 
     # Go Dark
     go_dark()
-    waitfordark()
     # Configure CSU
     setup_mask(mask)
+    waitfordark()
     execute_mask()
     waitfor_CSU()
-    # Set mode
-    set_obsmode(f"{filter}-spectroscopy")
-    # Open Hatch
-    open_hatch()
-    # Turn on dome flat lamps
-    turn_on_dome_flat_lamps()
-    # Take flats
-    exptime = float(cfg.get(filter, "flat_exptime", fallback=10))
-    for i in range(int(cfg.get(filter, "flat_count", fallback=9))):
-        take_exposure(exptime=exptime,
-                      coadds=int(cfg.get(filter, "flat_coadds", fallback=1)),
-                      sampmode=cfg.get(filter, "flat_sampmode", fallback='CDS'),
-                      wait=True)
-    # Turn off dome flat lamps
-    turn_off_dome_flat_lamps()
-    # Take lamp off flats
-    for i in range(int(cfg.get(filter, "flatoff_count", fallback=0))):
-        take_exposure(exptime=exptime,
-                      coadds=int(cfg.get(filter, "flat_coadds", fallback=1)),
-                      sampmode=cfg.get(filter, "flat_sampmode", fallback='CDS'),
-                      wait=True)
-    # Close hatch
-    go_dark()
-    close_hatch()
-    set_obsmode(f"{filter}-spectroscopy")
-    # Take Ne arcs
-    Ne_lamp('on')
-    for i in range(int(cfg.get(filter, "ne_count", fallback=0))):
-        take_exposure(exptime=exptime,
-                      coadds=int(cfg.get(filter, "ne_coadds", fallback=1)),
-                      sampmode=cfg.get(filter, "ne_sampmode", fallback='CDS'),
-                      wait=True)
-    Ne_lamp('off')
-    # Take Ar arcs
-    Ar_lamp('on')
-    for i in range(int(cfg.get(filter, "ar_count", fallback=0))):
-        take_exposure(exptime=exptime,
-                      coadds=int(cfg.get(filter, "ar_coadds", fallback=1)),
-                      sampmode=cfg.get(filter, "ar_sampmode", fallback='CDS'),
-                      wait=True)
-    Ar_lamp('off')
-    # Go Dark
-    go_dark()
+
+    for filt in filters:
+        hatch_posname = ktl.cache(service='mmdcs', keyword='POSNAME')
+        hatch_posname.monitor()
+        if hatch_posname == 'Closed':
+            # Start with Arcs
+            take_arcs(filt, cfg)
+            take_flats(filt, cfg)
+        elif hatch_posname == 'Open':
+            # Start with Flats
+            take_flats(filt, cfg)
+            take_arcs(filt, cfg)
 
     ##-------------------------------------------------------------------------
     ## Post-Condition Checks
@@ -158,3 +192,42 @@ def take_mask_calibrations(mask, filter, skipprecond=False, skippostcond=True):
     else:
         mechanisms_ok()
 
+    return None
+
+
+##-------------------------------------------------------------------------
+## Take Calibrations for All Masks
+##-------------------------------------------------------------------------
+def take_all_calibrations(masks, config='default_calibrations.cfg',
+                          skipprecond=False, skippostcond=True):
+    '''Loops over masks and takes calibrations for each.
+    
+    All masks must have the same calibration configuration file.
+    
+    Takes an input dictionary containing keys which are Mask objects (or
+    resolve to mask objects), and values which are a list of filters.
+    '''
+    this_function_name = inspect.currentframe().f_code.co_name
+    log.debug(f"Executing: {this_function_name}")
+
+    ##-------------------------------------------------------------------------
+    ## Pre-Condition Checks
+    if skipprecond is True:
+        log.debug('Skipping pre condition checks')
+    else:
+        pass
+
+    ##-------------------------------------------------------------------------
+    ## Script Contents
+
+    for entry in masks:
+        mask, filters = mask
+    
+    ##-------------------------------------------------------------------------
+    ## Post-Condition Checks
+    if skippostcond is True:
+        log.debug('Skipping post condition checks')
+    else:
+        pass
+
+    return None
