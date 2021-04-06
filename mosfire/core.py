1f005a697824c8d1d70befab7b29380f8b6a5a20
Josh Walawender <jmwalawender@gmail.com>
2021-04-06 10:47:43 -1000

    remove instruments

--- mosfire/core.py
+++ mosfire/core.py
@@ -6,12 +6,33 @@ import socket
 import subprocess
 import sys
 
-try:
-    import ktl
-except ModuleNotFoundError as e:
-    from instruments import dummy_ktl as ktl
+import ktl
 
-from instruments import create_log
+
+##-------------------------------------------------------------------------
+## Create logger object
+##-------------------------------------------------------------------------
+def create_log(name='MOSFIRE', loglevel=logging.INFO, logfile=None):
+    if type(loglevel) == str:
+        loglevel = getattr(logging, loglevel.upper())
+    log = logging.getLogger(name)
+    if len(log.handlers) == 0:
+        log.setLevel(logging.DEBUG)
+        ## Set up console output
+        LogConsoleHandler = logging.StreamHandler()
+        LogConsoleHandler.setLevel(loglevel)
+        LogFormat = logging.Formatter('%(asctime)s %(levelname)8s: %(message)s')
+        LogConsoleHandler.setFormatter(LogFormat)
+        log.addHandler(LogConsoleHandler)
+        ## Set up file output
+        if logfile is not None:
+            logfile = Path(logfile).expanduser()
+            if logfile.parent.exists() and logfile.parent.is_dir():
+                LogFileHandler = logging.FileHandler(logfile)
+                LogFileHandler.setLevel(logging.DEBUG)
+                LogFileHandler.setFormatter(LogFormat)
+                log.addHandler(LogFileHandler)
+    return log
 
 
 ##-------------------------------------------------------------------------
